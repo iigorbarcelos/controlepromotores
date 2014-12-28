586db99eb96190575ea8f1a621acb3a094b74d8b
Igor Barcelos <iigorbarcelos@gmail.com>
2014-12-28 20:04:00 -0200

    Comeco da classe de comunicacao biometria

---
+++ ControlePromotores/LeitorBiometrico.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NITGEN.SDK.NBioBSP;
+
+namespace ControlePromotores
+{
+    class LeitorBiometrico : Enumerable
+    {
+        //Inicializar o leitor   
+        m_
+
+
+            int i;
+            uint nNumDevice;
+            short[] nDeviceID;
+            uint ret = m_NBioAPI.EnumerateDevice(out nNumDevice, out nDeviceID);
+            if (ret == m_NBioAPI.Error.NONE)
+            {
+            comboDevice.Items.Add("Auto_Detect");
+            for (i = 0; i < nNumDevice; i++)
+            {
+            switch (nDeviceID[i])
+            {
+            case NBioAPI.Type.DEVICE_NAME.FDU04:
+            comboDevice.Items.Add("FDU04");
+            break;
+            case NBioAPI.Type.DEVICE_NAME.FDU14:
+            comboDevice.Items.Add("FDU14");
+            break;
+            }
+          }
+        }
+    }
+}
