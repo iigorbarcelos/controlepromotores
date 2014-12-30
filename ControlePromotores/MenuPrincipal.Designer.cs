26a0b0692268363c7e2cff3d27d3a3e4d2e60db2
Igor Barcelos <iigorbarcelos@gmail.com>
2014-12-30 12:14:21 -0200

    Alteracao da imagem de fundo do menu principal. Correcao de alguns componentes visuais que estavam com erro e desalinhados. Alteracao da dll de referencia para a nova passada pela nitgen.

--- ControlePromotores/MenuPrincipal.Designer.cs
+++ ControlePromotores/MenuPrincipal.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.MenuPainel = new System.Windows.Forms.Panel();
+            this.configuracoesLabel = new System.Windows.Forms.Label();
             this.configuracoesPictureBox = new System.Windows.Forms.PictureBox();
             this.relatorioLabel = new System.Windows.Forms.Label();
             this.relatorioPictureBox = new System.Windows.Forms.PictureBox();
@@ -37,7 +38,6 @@
             this.CadastrarPromotorPicture = new System.Windows.Forms.PictureBox();
             this.sairButton = new System.Windows.Forms.Button();
             this.tituloLabel = new System.Windows.Forms.Label();
-            this.configuracoesLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.MenuPainel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.configuracoesPictureBox)).BeginInit();
@@ -47,7 +47,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackgroundImage = global::ControlePromotores.Properties.Resources.checkinimg;
+            this.panel1.BackgroundImage = global::ControlePromotores.Properties.Resources.menuprincipal;
             this.panel1.Controls.Add(this.MenuPainel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -73,10 +73,25 @@
             this.MenuPainel.Size = new System.Drawing.Size(929, 635);
             this.MenuPainel.TabIndex = 1;
             // 
+            // configuracoesLabel
+            // 
+            this.configuracoesLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.configuracoesLabel.AutoSize = true;
+            this.configuracoesLabel.BackColor = System.Drawing.Color.Transparent;
+            this.configuracoesLabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.configuracoesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.configuracoesLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.configuracoesLabel.Location = new System.Drawing.Point(620, 382);
+            this.configuracoesLabel.Name = "configuracoesLabel";
+            this.configuracoesLabel.Size = new System.Drawing.Size(145, 24);
+            this.configuracoesLabel.TabIndex = 6;
+            this.configuracoesLabel.Text = "Configurações";
+            // 
             // configuracoesPictureBox
             // 
             this.configuracoesPictureBox.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.configuracoesPictureBox.Image = global::ControlePromotores.Properties.Resources.Configuration;
+            this.configuracoesPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.configuracoesPictureBox.Image = global::ControlePromotores.Properties.Resources.configuracoes;
             this.configuracoesPictureBox.Location = new System.Drawing.Point(620, 229);
             this.configuracoesPictureBox.Name = "configuracoesPictureBox";
             this.configuracoesPictureBox.Size = new System.Drawing.Size(145, 136);
@@ -88,19 +103,20 @@
             // 
             this.relatorioLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.relatorioLabel.AutoSize = true;
-            this.relatorioLabel.BackColor = System.Drawing.Color.White;
-            this.relatorioLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.relatorioLabel.BackColor = System.Drawing.Color.Transparent;
             this.relatorioLabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.relatorioLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.relatorioLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.relatorioLabel.Location = new System.Drawing.Point(403, 382);
             this.relatorioLabel.Name = "relatorioLabel";
-            this.relatorioLabel.Size = new System.Drawing.Size(105, 26);
+            this.relatorioLabel.Size = new System.Drawing.Size(103, 24);
             this.relatorioLabel.TabIndex = 4;
             this.relatorioLabel.Text = "Relatórios";
             // 
             // relatorioPictureBox
             // 
             this.relatorioPictureBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.relatorioPictureBox.BackColor = System.Drawing.Color.Transparent;
             this.relatorioPictureBox.Image = global::ControlePromotores.Properties.Resources.relatorios;
             this.relatorioPictureBox.Location = new System.Drawing.Point(383, 229);
             this.relatorioPictureBox.Name = "relatorioPictureBox";
@@ -113,13 +129,13 @@
             // 
             this.CadastrarPromotorLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.CadastrarPromotorLabel.AutoSize = true;
-            this.CadastrarPromotorLabel.BackColor = System.Drawing.Color.White;
-            this.CadastrarPromotorLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CadastrarPromotorLabel.BackColor = System.Drawing.Color.Transparent;
             this.CadastrarPromotorLabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.CadastrarPromotorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CadastrarPromotorLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.CadastrarPromotorLabel.Location = new System.Drawing.Point(148, 382);
             this.CadastrarPromotorLabel.Name = "CadastrarPromotorLabel";
-            this.CadastrarPromotorLabel.Size = new System.Drawing.Size(119, 26);
+            this.CadastrarPromotorLabel.Size = new System.Drawing.Size(117, 24);
             this.CadastrarPromotorLabel.TabIndex = 1;
             this.CadastrarPromotorLabel.Text = "Promotores";
             this.CadastrarPromotorLabel.Click += new System.EventHandler(this.CadastrarPromotorLabel_Click);
@@ -127,7 +143,7 @@
             // CadastrarPromotorPicture
             // 
             this.CadastrarPromotorPicture.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.CadastrarPromotorPicture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CadastrarPromotorPicture.BackColor = System.Drawing.Color.Transparent;
             this.CadastrarPromotorPicture.Image = global::ControlePromotores.Properties.Resources.imagemCadastrarPromotor;
             this.CadastrarPromotorPicture.Location = new System.Drawing.Point(135, 229);
             this.CadastrarPromotorPicture.Name = "CadastrarPromotorPicture";
@@ -162,20 +178,6 @@
             this.tituloLabel.Size = new System.Drawing.Size(539, 60);
             this.tituloLabel.TabIndex = 0;
             this.tituloLabel.Text = "Controle de Frequência\r\n";
-            // 
-            // configuracoesLabel
-            // 
-            this.configuracoesLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.configuracoesLabel.AutoSize = true;
-            this.configuracoesLabel.BackColor = System.Drawing.Color.White;
-            this.configuracoesLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.configuracoesLabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.configuracoesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.configuracoesLabel.Location = new System.Drawing.Point(620, 382);
-            this.configuracoesLabel.Name = "configuracoesLabel";
-            this.configuracoesLabel.Size = new System.Drawing.Size(147, 26);
-            this.configuracoesLabel.TabIndex = 6;
-            this.configuracoesLabel.Text = "Configurações";
             // 
             // MenuPrincipal
             // 
--- ControlePromotores/Properties/Resources.Designer.cs
+++ ControlePromotores/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace ControlePromotores.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap menuprincipal {
+            get {
+                object obj = ResourceManager.GetObject("menuprincipal", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap relatorios {
             get {
                 object obj = ResourceManager.GetObject("relatorios", resourceCulture);
