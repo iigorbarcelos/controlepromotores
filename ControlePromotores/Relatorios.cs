eb8bb5ce26e21e71dd4e2578175c9a0ad4f6f4c2
Igor Barcelos <iigorbarcelos@gmail.com>
2015-01-16 11:11:44 -0200

    finalizacao

--- ControlePromotores/Relatorios.cs
+++ ControlePromotores/Relatorios.cs
@@ -9,12 +9,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.Sql;
 using System.Data.SqlClient;
+using System.Collections;
 
 namespace ControlePromotores
 {
     public partial class Relatorios : Form
     {
 
+        ArrayList<Promotor> listaPromotores = new ArrayList<Promotor>();
+        
         public Relatorios()
         {
             InitializeComponent();
@@ -58,7 +61,61 @@ namespace ControlePromotores
 
         private void enviaRelatorioEmail_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(DateTime.Now.ToString());          
+            //Pega conexão com o banco
+            SqlConnection conn = new ConnectionFactory().getConnection();
+
+            //Cria um novo comando sql
+            SqlCommand command = new SqlCommand(";WITH Lancamentoshoras As ( " +
+
+"SELECT " +
+    "codpromotor, Data, hora, " +
+   "ROW_NUMBER() OVER ( " +
+        "PARTITION BY Data, codpromotor " +
+        "ORDER BY Data, codpromotor, hora) As Pos " +
+"FROM movpromotores), " +
+
+"LancamentosOrganizados As ( " +
+
+"SELECT " +
+    "L1.codpromotor, L1.Data, " +
+    "L1.hora As Entrada, L2.hora As Saida " +
+"FROM " +
+    "Lancamentoshoras As L1 " +
+    "INNER JOIN Lancamentoshoras As L2 ON " +
+        "L1.codpromotor = L2.codpromotor " +
+        "AND L1.Data = L2.Data " +
+        "AND L1.Pos = L2.Pos - 1 " +
+        "AND L1.Pos % 2 = 1) " +
+
+"SELECT codpromotor, datepart(WEEK,Data) as semana, " +
+    "RIGHT('0' + CAST(SUM(DateDiff(Mi,Entrada,Saida)) / 60 As VARCHAR(2)),2) + ':' + " +
+    "RIGHT('0' + CAST(SUM(DateDiff(Mi,Entrada,Saida)) % 60 As VARCHAR(2)),2) " +
+"As CargaHoraria " +
+
+"FROM LancamentosOrganizados " +
+"GROUP BY codpromotor, datepart(WEEK,Data)", conn);
+
+            try
+            {
+                SqlDataReader reader = command.ExecuteReader();
+
+                Promotor promotor = new Promotor();
+               
+                while (reader.Read())
+                {
+                    promotor
+                }
+            }
+            catch (SqlException exc)
+            {
+                MessageBox.Show("Erro ao consultar dados da semana \n" + exc);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            
+            
         }
     }
 }
